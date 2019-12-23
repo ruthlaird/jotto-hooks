b8208c6dcf36fe9c15801fd1041e836b58c730e1
Ruth Laird <ruth.laird@anaplan.com>
2019-12-23 20:19:17 +0000

    Added basic App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/**
+ * Setup function for App component.
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+}
+
+it('renders App without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
 });
