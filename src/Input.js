b34ba399f19095169e3c410feca6930b944faf12
Ruth Laird <ruth.laird@anaplan.com>
2019-12-23 20:35:49 +0000

    Added secretWord prop to Input

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Input() {
+function Input({ secretWord }) {
   return <div data-test='component-input'></div>;
 }
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
+};
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
 /**
  * Setup function for Input component.
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-    return shallow(<Input />);
-}
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
+};
 
 it('renders Input without error', () => {
-    const wrapper = setup();
-    const component = findByTestAttr(wrapper, 'component-input');
-    expect(component.length).toBe(1);
-  });
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-input');
+  expect(component.length).toBe(1);
+});
+
+it('does not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: 'party' });
+});
