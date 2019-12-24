d041df358e7fa29ebd28101f1f50c48dad1992af
Ruth Laird <ruth.laird@anaplan.com>
2019-12-24 11:19:26 +0000

    Added state code and tests to Input

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input({ secretWord }) {
-  return <div data-test='component-input'></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test='component-input'>
+      <form className='form-inline'>
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='Enter guess'
+          value={currentGuess}
+          onChange={event => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test='submit-button'
+          onClick={evt => {
+            evt.preventDefault();
+          }}
+          className='btn btn-primary mb-2'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
